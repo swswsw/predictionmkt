c207f9ac003c0cbcc82a4e60bf1bcb38081d60a9
sol wu <apiswswsw@gmail.com>
2018-08-22 00:55:22 -0700

    fix tests for multi market feature addition

--- test/testDistribute.js
+++ test/testDistribute.js
@@ -6,8 +6,8 @@ let mainapp = rewire('../app.js');
 test('test distribute for no-vote situation', function(t) {
   let txDistributeHandler = mainapp.__get__('txDistributeHandler');
 
-  let state = {"market1":{"id":1,"phaseTime":{},"bets":[{"amount":10,"outcome":1,"type":"bet","user":"alice"},{"amount":10,"outcome":2,"type":"bet","user":"bob"}],"oracles":[],"oracleOutcome":1,"challenge":{},"voteRecords":[],"payoutRatio":1.5},"balances":{"alice":9990,"bob":9990,"carol":10000}};
-  let tx = {"type": "distribute"};
+  let state = {"market":{"market1":{"id":"","phaseTime":{},"bets":[],"oracles":[],"oracleOutcome":-1,"voteRecords":[],"payoutRatio":1.5},"market2":{"id":"market2","phaseTime":{"marketStart":9,"marketEnd":3609,"oracleStart":3610,"oracleEnd":7210,"challengeStart":7211,"challengeEnd":10811,"voteStart":10812,"voteEnd":14412,"distributeStart":14413,"distributeEnd":18013},"bets":[{"amount":10,"marketId":"market2","outcome":1,"type":"bet","user":"alice"},{"amount":10,"marketId":"market2","outcome":2,"type":"bet","user":"bob"}],"oracles":[],"oracleOutcome":1,"voteRecords":[],"payoutRatio":1.5}},"balances":{"alice":9990,"bob":9990,"carol":10000}};
+  let tx = {"type": "distribute", "marketId": "market2"};
   let chainInfo = {height: 0}; // not used when we are in test mode that does not check for phase
 
   txDistributeHandler(state, tx, chainInfo);
@@ -23,61 +23,96 @@ test('test distribute for situation with vote', function(t) {
 
   let state = 
   {
-    "market1": {
-      "id": 1,
-      "phaseTime": {
-        
+    "market": {
+      "market1": {
+        "id": "",
+        "phaseTime": {
+          
+        },
+        "bets": [
+          
+        ],
+        "oracles": [
+          
+        ],
+        "oracleOutcome": -1,
+        "voteRecords": [
+          
+        ],
+        "payoutRatio": 1.5
       },
-      "bets": [
-        {
-          "amount": 10,
-          "outcome": 1,
-          "type": "bet",
+      "market2": {
+        "id": "market2",
+        "phaseTime": {
+          "marketStart": 13,
+          "marketEnd": 3613,
+          "oracleStart": 3614,
+          "oracleEnd": 7214,
+          "challengeStart": 7215,
+          "challengeEnd": 10815,
+          "voteStart": 10816,
+          "voteEnd": 14416,
+          "distributeStart": 14417,
+          "distributeEnd": 18017
+        },
+        "bets": [
+          {
+            "amount": 10,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "bet",
+            "user": "alice"
+          },
+          {
+            "amount": 10,
+            "marketId": "market2",
+            "outcome": 2,
+            "type": "bet",
+            "user": "bob"
+          }
+        ],
+        "oracles": [
+          
+        ],
+        "oracleOutcome": 2,
+        "challenge": {
+          "amount": 100,
+          "marketId": "market2",
+          "type": "challenge",
           "user": "alice"
         },
-        {
-          "amount": 10,
-          "outcome": 2,
-          "type": "bet",
-          "user": "bob"
-        }
-      ],
-      "oracles": [
-        
-      ],
-      "oracleOutcome": 2,
-      "challenge": {
-        "amount": 100,
-        "type": "challenge",
-        "user": "alice"
-      },
-      "voteRecords": [
-        {
-          "amount": 1000,
-          "outcome": 1,
-          "type": "vote",
-          "user": "alice"
-        },
-        {
-          "amount": 10,
-          "outcome": 2,
-          "type": "vote",
-          "user": "bob"
-        },
-        {
-          "amount": 1000,
-          "outcome": 1,
-          "type": "vote",
-          "user": "carol"
-        },
-        {
-          "amount": 1000,
-          "outcome": 1,
-          "type": "vote",
-          "user": "alice"
-        }
-      ],
-      "payoutRatio": 1.5
+        "voteRecords": [
+          {
+            "amount": 1000,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "vote",
+            "user": "alice"
+          },
+          {
+            "amount": 10,
+            "marketId": "market2",
+            "outcome": 2,
+            "type": "vote",
+            "user": "bob"
+          },
+          {
+            "amount": 1000,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "vote",
+            "user": "carol"
+          },
+          {
+            "amount": 1000,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "vote",
+            "user": "alice"
+          }
+        ],
+        "payoutRatio": 1.5
+      }
     },
     "balances": {
       "alice": 7890,
@@ -87,7 +122,7 @@ test('test distribute for situation with vote', function(t) {
   }
   ;
 
-  let tx = {"type": "distribute"};
+  let tx = {"type": "distribute", "marketId": "market2"};
   let chainInfo = {height: 0}; // not used when we are in test mode that does not check for phase
 
   txDistributeHandler(state, tx, chainInfo);
--- test/testPayout.js
+++ test/testPayout.js
@@ -7,9 +7,9 @@ test('test doPayout for bets', function(t) {
   let doPayout = mainapp.__get__('doPayout');
 
   let bets = [{"amount":10,"outcome":1,"type":"bet","user":"alice"},{"amount":10,"outcome":2,"type":"bet","user":"bob"}];
-  let state = {"market1":{"id":1,"phaseTime":{},"bets":[{"amount":10,"outcome":1,"type":"bet","user":"alice"},{"amount":10,"outcome":2,"type":"bet","user":"bob"}],"oracles":[],"oracleOutcome":-1,"challenge":{},"voteRecords":[],"payoutRatio":1.5},"balances":{"alice":9990,"bob":9990,"carol":10000}};
+  let state = {"market":{"market1":{"id":"","phaseTime":{},"bets":[],"oracles":[],"oracleOutcome":-1,"voteRecords":[],"payoutRatio":1.5},"market2":{"id":"market2","phaseTime":{"marketStart":9,"marketEnd":3609,"oracleStart":3610,"oracleEnd":7210,"challengeStart":7211,"challengeEnd":10811,"voteStart":10812,"voteEnd":14412,"distributeStart":14413,"distributeEnd":18013},"bets":[{"amount":10,"marketId":"market2","outcome":1,"type":"bet","user":"alice"},{"amount":10,"marketId":"market2","outcome":2,"type":"bet","user":"bob"}],"oracles":[],"oracleOutcome":1,"voteRecords":[],"payoutRatio":1.5}},"balances":{"alice":9990,"bob":9990,"carol":10000}};
 
-  doPayout("bet", 1, bets, state);
+  doPayout("bet", 1, bets, state, "market2");
 
   t.equals(state.balances.alice, 10010);
   t.equals(state.balances.bob, 9990);
@@ -51,61 +51,96 @@ test('test doPayout for votes', function(t) {
 
   let state = 
   {
-    "market1": {
-      "id": 1,
-      "phaseTime": {
-        
+    "market": {
+      "market1": {
+        "id": "",
+        "phaseTime": {
+          
+        },
+        "bets": [
+          
+        ],
+        "oracles": [
+          
+        ],
+        "oracleOutcome": -1,
+        "voteRecords": [
+          
+        ],
+        "payoutRatio": 1.5
       },
-      "bets": [
-        {
-          "amount": 10,
-          "outcome": 1,
-          "type": "bet",
+      "market2": {
+        "id": "market2",
+        "phaseTime": {
+          "marketStart": 13,
+          "marketEnd": 3613,
+          "oracleStart": 3614,
+          "oracleEnd": 7214,
+          "challengeStart": 7215,
+          "challengeEnd": 10815,
+          "voteStart": 10816,
+          "voteEnd": 14416,
+          "distributeStart": 14417,
+          "distributeEnd": 18017
+        },
+        "bets": [
+          {
+            "amount": 10,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "bet",
+            "user": "alice"
+          },
+          {
+            "amount": 10,
+            "marketId": "market2",
+            "outcome": 2,
+            "type": "bet",
+            "user": "bob"
+          }
+        ],
+        "oracles": [
+          
+        ],
+        "oracleOutcome": 2,
+        "challenge": {
+          "amount": 100,
+          "marketId": "market2",
+          "type": "challenge",
           "user": "alice"
         },
-        {
-          "amount": 10,
-          "outcome": 2,
-          "type": "bet",
-          "user": "bob"
-        }
-      ],
-      "oracles": [
-        
-      ],
-      "oracleOutcome": 2,
-      "challenge": {
-        "amount": 100,
-        "type": "challenge",
-        "user": "alice"
-      },
-      "voteRecords": [
-        {
-          "amount": 1000,
-          "outcome": 1,
-          "type": "vote",
-          "user": "alice"
-        },
-        {
-          "amount": 10,
-          "outcome": 2,
-          "type": "vote",
-          "user": "bob"
-        },
-        {
-          "amount": 1000,
-          "outcome": 1,
-          "type": "vote",
-          "user": "carol"
-        },
-        {
-          "amount": 1000,
-          "outcome": 1,
-          "type": "vote",
-          "user": "alice"
-        }
-      ],
-      "payoutRatio": 1.5
+        "voteRecords": [
+          {
+            "amount": 1000,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "vote",
+            "user": "alice"
+          },
+          {
+            "amount": 10,
+            "marketId": "market2",
+            "outcome": 2,
+            "type": "vote",
+            "user": "bob"
+          },
+          {
+            "amount": 1000,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "vote",
+            "user": "carol"
+          },
+          {
+            "amount": 1000,
+            "marketId": "market2",
+            "outcome": 1,
+            "type": "vote",
+            "user": "alice"
+          }
+        ],
+        "payoutRatio": 1.5
+      }
     },
     "balances": {
       "alice": 7890,
@@ -115,7 +150,7 @@ test('test doPayout for votes', function(t) {
   }
   ;
 
-  doPayout("vote", 1, votes, state);
+  doPayout("vote", 1, votes, state, "market2");
 
   // alice balance should be ~9896.66666666666666666666
   // tbd: worry about rounding
