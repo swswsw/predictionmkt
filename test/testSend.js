8b1ce6f42397e6425f46eaca6851fc52baa62839
sol wu <apiswswsw@gmail.com>
2018-09-15 13:26:37 -0700

    fix test case that is broken by #29 sequence requirement

--- test/testSend.js
+++ test/testSend.js
@@ -5,21 +5,32 @@ let { post, get } = require('axios');
 let secp = require('secp256k1');
 let { sha256, addressHash } = require('../common.js');
 let getSigHash = require('../sigHash.js');
+let test = require('tape');
 
 //let priv = sha256('463852363a38c0105fc1169995696fb484ec11710ba174b3994c50baa5159423'); // sample private key
 let priv = Buffer.from("463852363a38c0105fc1169995696fb484ec11710ba174b3994c50baa5159423", "hex"); // bob's private key
 let pub = secp.publicKeyCreate(priv); // create public key
-console.log(addressHash(pub)); // create address
+let addr = addressHash(pub);
+console.log("addr: ", addr); // create address
 
 let toAddr = "5wvwWgKP3Qfw1akQoXWg4NtKmzx5v4dTj";
 
 async function main () {
+
+  let state = (await get('http://localhost:3000/state')).data;
+  console.log("state: ", state);
+  let sequence = state.seq[addr];
+  if (typeof sequence === "undefined") {
+    sequence = 0;
+  }
+  console.log("sequence: ", sequence);
+
   let tx = {
     type: "send",
     from: {
       amount: 10,
       pubkey: pub,
-      sequence: 0
+      sequence: sequence,
     },
     to: {
       address: toAddr,
@@ -41,6 +52,10 @@ async function main () {
   let res = await post('http://localhost:3000/txs', tx);
   console.log("tx resp", res.data);
 
-  
+  test("tx valid", function(t){
+    t.notEqual(res.data.result.height, 0, "response is valid");
+    t.end();
+  });
+
 }
 main()
--- test/testVerifySig.js
+++ test/testVerifySig.js
@@ -1,22 +1,33 @@
 /**
  * a sample client
  */
-let { post } = require('axios');
+let { post, get } = require('axios');
 let secp = require('secp256k1');
 let { sha256, addressHash } = require('../common.js');
 let getSigHash = require('../sigHash.js');
+let test = require('tape');
 
 //let priv = sha256('463852363a38c0105fc1169995696fb484ec11710ba174b3994c50baa5159423'); // sample private key
 let priv = Buffer.from("463852363a38c0105fc1169995696fb484ec11710ba174b3994c50baa5159423", "hex"); // bob's private key
 let pub = secp.publicKeyCreate(priv); // create public key
-console.log(addressHash(pub)); // create address
+let addr = addressHash(pub);
+console.log("addr: ", addr); // create address
 
 async function main () {
+
+  let state = (await get('http://localhost:3000/state')).data;
+  console.log("state: ", state);
+  let sequence = state.seq[addr];
+  if (typeof sequence === "undefined") {
+    sequence = 0;
+  }
+  console.log("sequence: ", sequence);
+
   let tx = {
     type: "verifySig",
     from: {
       pubkey: pub,
-      sequence: 0
+      sequence: sequence,
     },
     test: "test", // this is not needed.  just showing that adding new property will still allow signature to be verified
     to: {
@@ -35,9 +46,14 @@ async function main () {
   let sigHex = signature.toString('hex');
   console.log("signature hex: ", sigHex);
 
+
   let res = await post('http://localhost:3000/txs', tx);
   console.log("tx resp", res.data);
-
+  
+  test("tx valid", function(t){
+    t.notEqual(res.data.result.height, 0, "response is valid");
+    t.end();
+  });
 
   // verify signature
   if (!secp.verify(sigHash, signature, pub)) {
