40ad7995f26e81679b4be5b299e839bd571f2c54
Peter Ma <peterma@Peters-MacBook-Pro-2.local>
2018-09-27 23:03:34 -0700

    seperating web.js and app.js

--- app.js
+++ app.js
@@ -4,43 +4,6 @@ let secp = require('secp256k1');
 let { sha256, addressHash } = require('./common.js');
 let getSigHash = require('./sigHash.js');
 let stateUtil = require('./stateUtil.js');
-let express = require('express');
-
-
-//Peter: Express
-const expressapp = express();
-expressapp.set('view engine', 'pug');
-expressapp.use(express.static(__dirname + '/public'));
-expressapp.set('views', __dirname + '/public');
-expressapp.engine('html', require('ejs').renderFile);
-expressapp.set('view engine', 'html');
-
-expressapp.get('/', (req, res) => {
-  res.render('index.html');
-});
-
-expressapp.get('/login', (req, res) => {
-  res.render('login.html');
-});
-
-expressapp.get('/market', (req, res) => {
-  res.render('market.html');
-});
-
-expressapp.get('/create', (req, res) => {
-  res.render('create.html');
-});
-
-
-expressapp.get('/profile', (req, res) => {
-  res.render('profile.html');
-});
-
-expressapp.listen(80, () => {
-  //console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
-});
-
 
 const STARTING_SEQ = 0;
 
@@ -156,11 +119,9 @@ app.use(txVoteHandler);
 app.use(txDistributeHandler);
 app.use(txSendHandler);
 
-/*
 app.listen(3000).then(function(appInfo) {
   console.log(appInfo);
 })
-*/
 
 /**
  * doesn't do anything other than verify signature.  
